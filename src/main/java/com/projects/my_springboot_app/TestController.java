d7436ddef1e96fa7342529c088cd145c8d9ca310
Shatabdi Panda <Shatabdi.Panda@Triconinfotech.com>
2024-06-12 15:58:37 +0530

    first commit

---
+++ src/main/java/com/projects/my_springboot_app/TestController.java
@@ -0,0 +1,14 @@
+package com.projects.my_springboot_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Springboot!";
+    }
+
+}
